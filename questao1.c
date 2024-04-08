555c63755fcc8ac41b6bc55bf9cde4b967092a7b
Gustavo <gustavo.l.mav@gmail.com>
2024-04-08 10:57:11 -0300

    add questao1

---
+++ questao1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int indice = 13;
+    int soma = 0;
+    for(int i=0;i<indice;i++){
+        i = i+1;
+        soma = soma + i;
+    } //Soma de números ímpares de a até 13
+    printf("O valor da soma = %d\n",soma);
+}
