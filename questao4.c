fa3f13242b832be943787c9b15a02a41e68e7ebf
Gustavo <gustavo.l.mav@gmail.com>
2024-04-08 11:58:12 -0300

    questao4

---
+++ questao4.c
@@ -0,0 +1,4 @@
+/*Com as lampâdas estando na mesma sala, basta acender um interruptor e ver qual
+se acendeu, ao voltar desligo a que estava ligado e ligo um interruptor diferente,
+volto a sala e vejo qual se ligou, com isso, sobra apenas um interruptor para uma
+lampâda.*/
